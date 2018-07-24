b8f7dc3fedee4ff6d14d4349a47a9a637eefc139
prasasaw <34060802+prasasaw@users.noreply.github.com>
2018-07-24 15:34:22 +0200

    Example function calls for text extraction and parsing
    Example function calls for text extraction and parsing

---
+++ sample.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 13 12:01:19 2018
+
+@author: prasasaw
+"""
+import resume_parser
+import file_to_text
+import glob
+import os
+
+dir =  r"C:\Resume Miner1"
+files = glob.glob(os.path.join(dir,"*"))  
+    
+for filename in files:
+
+    resumetext = file_to_text.text_converter(filename)
+    output = resume_parser.parse_resume(resumetext)
+
